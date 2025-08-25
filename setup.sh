dcbe4c809b840e5aab1e45978920ea1fcf1af152
Nikita Serdyukov <serdiukovn@gmail.com>
2025-08-25 16:54:26 +0300

    upd

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+PLAYBOOK=$1
+
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+rm get-pip.py
+pip install ansible
+sudo dnf install -y git
+
+git clone https://github.com/nserd/workstation_setup.git /tmp/ws_setup
+cd /tmp/ws_setup
+ansible-galaxy install -r requirements.yml
+ansible-playbook $PLAYBOOK -D
