0820b135638c58d54f47df7044d722109d1fe9ab
Nikita Serdyukov <serdiukovn@gmail.com>
2025-08-27 00:55:14 +0300

    edit setup script

--- setup.sh
+++ setup.sh
@@ -4,14 +4,15 @@ set -e
 PLAYBOOK=$1
 LOCAL_REPO_PATH="$HOME/.ws_setup"
 
+rm -rf $LOCAL_REPO_PATH
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 rm get-pip.py
 pip install ansible
 sudo dnf install -y git python3-libdnf5
 
-git clone https://github.com/nserd/workstation_setup.git $HOME/.ws_setup
-cd $HOME/.ws_setup
+git clone https://github.com/nserd/workstation_setup.git $LOCAL_REPO_PATH
+cd $LOCAL_REPO_PATH
 ansible-galaxy install -r requirements.yml
 
 shift
