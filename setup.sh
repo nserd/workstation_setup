b43a2d8903e8e83c67b560d3b2f5c2ca0ad4b977
Nikita Serdyukov <serdiukovn@gmail.com>
2025-08-27 00:53:30 +0300

    fix

--- setup.sh
+++ setup.sh
@@ -4,19 +4,15 @@ set -e
 PLAYBOOK=$1
 LOCAL_REPO_PATH="$HOME/.ws_setup"
 
-if [ ! -d "$LOCAL_REPO_PATH" ]; then
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python get-pip.py
-    rm get-pip.py
-    pip install ansible
-    sudo dnf install -y git python3-libdnf5
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+rm get-pip.py
+pip install ansible
+sudo dnf install -y git python3-libdnf5
 
-    git clone https://github.com/nserd/workstation_setup.git $HOME/.ws_setup
-    cd $HOME/.ws_setup
-    ansible-galaxy install -r requirements.yml
-else
-    cd $HOME/.ws_setup
-fi
+git clone https://github.com/nserd/workstation_setup.git $HOME/.ws_setup
+cd $HOME/.ws_setup
+ansible-galaxy install -r requirements.yml
 
 shift
 ansible-playbook $PLAYBOOK -D $@
